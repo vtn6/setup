bdf30a43a02497e9feeb0a6e29b0443e2cbd71d8
vtn6 <vtn6@cornell.edu>
2013-07-19 09:34:23 -0400

    Update setup.sh
    added npm install express

--- setup.sh
+++ setup.sh
@@ -17,6 +17,8 @@ nvm use v0.10.12
 # http://jshint.com/
 npm install -g jshint
 
+npm install express
+
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
